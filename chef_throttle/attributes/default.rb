d5f64dc6fe11ea17dba615aa41bb5caf770a5e40
Jack Foy <jfoy@whitepages.com>
2013-10-15 18:00:06 -0700

    v1.0.2

--- chef_throttle/attributes/default.rb
+++ chef_throttle/attributes/default.rb
@@ -4,5 +4,5 @@
 default[:chef_throttle][:run_on_failure] = false
 default[:chef_throttle][:limit] = 1
 default[:chef_throttle][:cluster_name] = "default_cluster"
-default[:chef_throttle][:cluster_path] = "/chef_throttle/clusters"
+default[:chef_throttle][:cluster_path] = "chef_throttle/clusters"
 default[:chef_throttle][:lock_path] = "/queue"
